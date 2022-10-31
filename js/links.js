783dc84e3303a69f50c71dbe0d2799e0f21920d4
lee-mihye <gnsap@naver.com>
2022-10-31 12:34:14 +0900

    annexcombine_3

--- js/links.js
+++ js/links.js
@@ -53,6 +53,11 @@ $(function () { /////// jQB ///////////////////////
     // 로고이미지 클릭하면 index.html로 이동
     $(".event_wrap .logo a").click(function (e) {
         e.preventDefault();
+       location.href = "./index.html";
+    }); ////////// click //////////////
+    // 로고이미지 클릭하면 index.html로 이동
+    $(".event_article_wrap .logo a").click(function (e) {
+        e.preventDefault();
        location.href = "../index.html";
     }); ////////// click //////////////
 
